366faee8129f4a18cfbc534f8527b51b634f0fd8
Kseol <krendec@gmail.com>
2015-11-18 16:05:37 +0300

    first

---
+++ elpa/auto-capitalize-20131013.2205/auto-capitalize-autoloads.el
@@ -0,0 +1,18 @@
+;;; auto-capitalize-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("auto-capitalize-pkg.el" "auto-capitalize.el")
+;;;;;;  (22089 56289 511359 706000))
+
+;;;***
+
+(provide 'auto-capitalize-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; auto-capitalize-autoloads.el ends here
---
+++ elpa/auto-capitalize-20131013.2205/auto-capitalize-pkg.el
@@ -0,0 +1,1 @@
+(define-package "auto-capitalize" "20131013.2205" "Automatically capitalize (or upcase) words" (quote nil))
